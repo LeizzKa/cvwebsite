9a21368a59eb7c1caaea8645162c6681cc43c405
Leizz <lluukkon@paivola.fi>
2022-10-28 22:31:45 +0300

    Add some raw data into App.js

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,18 +1,57 @@
 import './App.css';
 
 
-function App() {
+function Education() {
+  let education = {
+    lukio: "Päivölän Opisto",s
+  }
+  return(
+    <div>
+      <h1>Education</h1>
+      <p>Highschool: {education.lukio}</p>
+    </div>
+  )
+}
+function AboutMe() {
+  let hobbies = () => (
+    <div>
+      <p>Chess</p>
+      <p>Riichi Mahjong</p>
+      <p>Role Playing</p>
+      <p>Video Games</p>
+    </div>
+  )
+  let about_me = {
+    full_name: "Leevi Mikael Luukkonen",
+    nickname: "Leizz",
+    age: "20",
+    personality: "",
+    hobbies: hobbies
+  }
+ 
+  return (
+    <div>
+      <p>Name:</p>
+      {about_me.full_name}
+      <br/>
+      <p>Nickname:</p>
+      {about_me.nickname}
+      <br/>
+      <p>Age</p>
+      {about_me.age}
+      <br/>
+      <h1>Hobbies</h1>
+      {about_me.hobbies()}
+    </div>
+  )
+}
+
+
+function App(props) {
   return ( 
     <div>
-      <h1>
-        About Me
-      </h1>
-      <p>
-        20 years old
-      </p>
-      <p1>
-      2 years of full stack experience
-      </p1>
+     <AboutMe about_me={props.about_me} />
+     <Education/>
     </div>
   );
 }
