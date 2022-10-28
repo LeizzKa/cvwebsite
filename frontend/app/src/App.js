12d2312a7ef3caa206605feae894862d18dc8244
Leizz <lluukkon@paivola.fi>
2022-10-28 21:02:57 +0300

    Create raw working react front

---
+++ frontend/app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App() {
+  return ( 
+    <div>
+      <h1>
+        About Me
+      </h1>
+      <p>
+        20 years old
+      </p>
+      <p1>
+      2 years of full stack experience
+      </p1>
+    </div>
+  );
+}
+
+export default App;
