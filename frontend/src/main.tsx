85a7f47989370a59d9119657ca2d09f5dc90bd62
Leizz Luukkonen <l.luukkonen@outlook.com>
2023-01-05 07:08:52 +0200

    Add functionality to fetch data from backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PersonalInfo from './components/personalInfo'
-import Navbar from './components/navbar'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PersonalInfo from "./components/personalInfo";
+import Navbar from "./components/navbar";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Navbar/>
-    <PersonalInfo/>
+    <Navbar />
+    <PersonalInfo />
   </React.StrictMode>
-)
+);
