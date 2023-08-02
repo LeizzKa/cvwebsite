9168ac903c7fed9505b65a542000ed0d8822ed0d
Leizz Luukkonen <l.luukkonen@outlook.com>
2023-08-02 13:31:06 +0300

    Now able to fetch data from API

--- backend/app/src/api.py
+++ backend/app/src/api.py
@@ -1,17 +1,25 @@
-from fastapi import FastAPI
-from db import Database
 import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from db import Database
 import uvicorn
 
-
+origins = [
+    "http://localhost:5173"
+]
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get("/data")
 async def get_all():
     data = Database.data_query()
     print("Data received successfully!")
-    return json.dumps(data)
+    return json.dumps(data, default=str)
 
 
 if __name__ == "__main__":
--- frontend/src/components/personalInfo.tsx
+++ frontend/src/components/personalInfo.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import '../App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
 
 function PersonalInfo() {
-   let about_me = {
-    name: "Leevi Mikael Luukkonen",
-    personality: "Placeholder",
-    experience: "Placeholder",
-    hobbies: "Placeholder"
-  }
+  const [data, setData] = useState();
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:5000/data");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="me">
       <img src="src/assets/cvkuva2.jpg" alt="" />
+      <p>{data}</p>
     </div>
-  )
+  );
 }
 
-export default PersonalInfo
+export default PersonalInfo;
